b07ae64d5ca2be4ff7f61dbca3dd84f73d53d1eb
RanjithKumar <ranjithkumar.sv@infovision.com>
2022-01-21 16:57:57 +0530

    new fileds

--- scripts/index.js
+++ scripts/index.js
@@ -2,10 +2,12 @@ document.getElementById("detailsForm").addEventListener("submit", submitForm);
 
 function submitForm(e) {
   e.preventDefault();
+  var name = getInputVal("name");
+  var phone = getInputVal("contactNumber");
   var reason = getInputVal("callRequestReason");
-  var comments = getInputVal("comments");
-  localStorage.setItem("callReason", reason);
-  localStorage.setItem("additionalComments", comments);
+  localStorage.setItem("customerName", name);
+  localStorage.setItem("customerPhone", phone);
+  localStorage.setItem("callReason", reason);;
   window.location.replace("./customerCall.html");
 }
 
@@ -13,3 +15,14 @@ function submitForm(e) {
 function getInputVal(id) {
   return document.getElementById(id).value;
 }
+
+//block texts in phone input
+function onlyNumberKey(evt) {
+    // Only ASCII character in that range allowed
+    var ASCIICode = evt.which ? evt.which : evt.keyCode;
+    if (ASCIICode === 43) {
+      //allow + operator
+      return true;
+    } else if (ASCIICode > 31 && (ASCIICode < 48 || ASCIICode > 57)) return false; //dont allow lettersor symbols
+    return true;
+  }
